b319596f003dfbc63bd5fc1719e7825786ec693b
Bankole Esan <techybanky@gmail.com>
2024-05-01 11:21:56 +0100

    fix: export new gateway reponse format

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { Rez } from './serviceResponses';
 export { Rez, statuses, StatusType, IDataAccess, TFuncResult, TResponseData, TStatus, TStatusType } from './serviceResponses';
 export { ServiceResponse } from './@types/ServiceResponse';
 export { ServiceEvent } from './@types/ServiceEvent';
-const { BadRequest, ExpectationFailed, Forbidden, InternalServerError, MethodNotAllowed, NotFound, OK, ServiceUnavailable, TimeoutError, TooManyRequests, UnProcessableEntity, UnSupportedMediaType, Unauthorized } = Rez
-export { BadRequest, ExpectationFailed, Forbidden, InternalServerError, MethodNotAllowed, NotFound, OK, ServiceUnavailable, TimeoutError, TooManyRequests, UnProcessableEntity, UnSupportedMediaType, Unauthorized }
+const { BadRequest, ExpectationFailed, Forbidden, InternalServerError, MethodNotAllowed, NotFound, OK, ServiceUnavailable, TimeoutError, TooManyRequests, UnProcessableEntity, UnSupportedMediaType, Unauthorized, GatewayTimeout } = Rez
+export { BadRequest, ExpectationFailed, Forbidden, InternalServerError, MethodNotAllowed, NotFound, OK, ServiceUnavailable, TimeoutError, TooManyRequests, UnProcessableEntity, UnSupportedMediaType, Unauthorized, GatewayTimeout }
 console.table({name: '@neoncoder/service-response-formatter', version: "0.0.2", github: 'https://github.com/Bankole2000/service-reponse-formatter.git'})
